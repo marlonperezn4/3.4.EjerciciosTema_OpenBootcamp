a14639a816b04eed835635fa59d4453e968f247c
marlonperezdev <marfrape13@gmail.com>
2023-04-26 10:53:01 -0500

    Primer ejercicio

---
+++ PrimeraParte/BasicFunctions/src/Main.java
@@ -0,0 +1,17 @@
+// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
+// then press Enter. You can now see whitespace characters in your code.
+public class Main {
+    public static void main(String[] args) {
+        //Se define el tipo velor de la variable
+        int resultado;
+        // llamar a la función
+        resultado = sumarNumeros(15, 9, 48);
+        //Mostrar resultado
+        System.out.println(resultado);
+    }
+    //Se crea la función
+    public static int sumarNumeros(int x, int y, int z){
+        //Se suma y retorna el resultado
+        return x + y + z;
+    }
+}
