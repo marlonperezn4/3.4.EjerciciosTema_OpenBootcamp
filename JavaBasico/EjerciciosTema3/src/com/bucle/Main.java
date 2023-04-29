8146125774f8a2f47cb6b6c5ff7af7c3bf43825c
marlonperezdev <marfrape13@gmail.com>
2023-04-29 01:27:14 -0500

    Ejercicios Tema 3 JavaBasico

---
+++ JavaBasico/EjerciciosTema3/src/com/bucle/Main.java
@@ -0,0 +1,15 @@
+package com.bucle;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        String[] nombres= new String[]{"Carla", "Pacho", "Elsa"};
+
+        for ( String nombre : nombres ) {
+            System.out.print(nombre);
+        }
+
+    }
+
+}
