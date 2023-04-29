b3d50e0c167ecd6d8d86744e0e7a98d51b258420
marlonperezdev <marfrape13@gmail.com>
2023-04-28 23:14:59 -0500

    Ejercicios Tema 1 JavaBasico

---
+++ JavaBasico/EjerciciosTema1/src/com/tiposdedatos/Main.java
@@ -0,0 +1,44 @@
+package com.tiposdedatos;
+
+public class Main {
+    public static void main(String[] args) {
+        //Tipos de datos
+
+
+        //Cadena
+        String cadena = "Hola";
+        //Caracter
+        char caracter =  'C';
+
+        //Booleano
+        boolean verdadero = true;
+        boolean falso = false;
+
+        //Float decimal
+        float x = 4.4f;
+        //Double decimal
+        double y = 4.99d;
+
+        //Entero
+        int numeroint = 1444;
+        //Long
+        long numerolong = 1111111111;
+        //Byte
+        byte numerobyte = 127;
+        //Short
+        short numerohshort = 200;
+
+        System.out.println("Tipos de Datos");
+        System.out.println("String: " + cadena);
+        System.out.println("Caracter: " + caracter);
+        System.out.println("Booleano: " + verdadero);
+        System.out.println("Booleano: " + falso);
+        System.out.println("Float: " + x);
+        System.out.println("Double: " + y);
+        System.out.println("Entero: " + numeroint);
+        System.out.println("Long: " + numerolong);
+        System.out.println("Byte: " + numerobyte);
+        System.out.println("Short: " + numerohshort);
+
+    }
+}
