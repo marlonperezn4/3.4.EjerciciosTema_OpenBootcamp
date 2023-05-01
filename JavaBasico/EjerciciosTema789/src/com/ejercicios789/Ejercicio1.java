e73cb028ddcba1828af17d1deae184af6d36a5a6
marlonperezdev <marfrape13@gmail.com>
2023-04-30 22:55:04 -0500

    Ejercicio 1 Tema 6, 7 y 8 JavaBasico

---
+++ JavaBasico/EjerciciosTema789/src/com/ejercicios789/Ejercicio1.java
@@ -0,0 +1,17 @@
+package com.ejercicios789;
+
+public class Ejercicio1 {
+        public static void main(String[] args){
+            System.out.println("###################################");
+            System.out.println("##### Vector Unidimensional #######");
+            System.out.println("###################################");
+            System.out.println("");
+
+            String miFamilia[] = {"Marlon ", "Marlan", "Marlen", "Marlin", "Marlun"};
+
+            for (int i = 0; i < miFamilia.length; i++) {
+                System.out.println("Posición: "+i+" Nombre: "+miFamilia[i]);
+            }
+
+        }
+}
